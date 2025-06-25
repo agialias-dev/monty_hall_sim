e9f6fd28d168ea3d0b0499bcd89e08a47eb9a629
agialias <agialias4000@gmail.com>
2025-06-25 15:26:26 +0100

    Multiple arguments available, now using Argparse, simulator is now fully funcitoning CLI tool.

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-import sys, getopt
+import argparse
 from round import play_round
 
 def main():
-    args = sys.argv[1:]
-    short_options = 'v'
-    long_options = ['verbose', 'trials=']
-    optlist, args = getopt.getopt(args, short_options, long_options)
-    if any([opt[0]=='-v' or opt[0]=='--verbose' for opt in optlist]):
-        verbose = True
-    else:
-        verbose = False
+#Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose",
+                        help="Increases output verbosity, level 1 displays the host and contestants chat and level 2 displays repr information on the objects involved (e.g. -v 1, --verbose 2)",
+                        type=int, choices=[1, 2])
+    parser.add_argument("-i", "--iterations", type=int, default=1, help="Number of iterations to run (e.g. -i 10, --iterations 100).")
+    args = parser.parse_args()
 
-    play_round(True, verbose)
+# Run simulations based on arguments provided.
+    switch_wins = 0
+    keep_wins = 0
+    i = 1
+    while i <= args.iterations:
+        sw = play_round(True, args.verbose)
+        i += 1
+        if sw == True:
+            switch_wins += 1
     print("----------------------------------------------------------------------------------")
-    play_round(False, verbose)
-    print()
+    i = 1
+    while i <= args.iterations:
+        kw = play_round(False, args.verbose)
+        i += 1
+        if kw == True:
+            keep_wins += 1
+
+#Display the results.
+    print(f"Switching the box resulted in {switch_wins} wins in {args.iterations} iterations.")
+    print(f"Keeping the box resulted in {keep_wins} wins in {args.iterations} iterations.")
     return
 
 if __name__ == "__main__":
--- round.py
+++ round.py
@@ -1,4 +1,4 @@
-import random, sys, getopt
+import random
 from boxes import Box
 from players import Player
 
@@ -8,56 +8,66 @@ def play_round(player_type, verbose):
 #   verbose (bool): if enabled prints enhanced information.
     
 #initialization
-    if verbose == True:
-        def vprint(print_data):
+    if verbose == 1:
+        def v1print(print_data):
+            print(print_data)
+        def v2print(print_data):
+            pass
+    elif verbose == 2:
+        def v1print(print_data):
+            print(print_data)
+        def v2print(print_data):
             print(print_data)
     else:
-        def vprint(print_data):
+        def v1print(print_data):
+            pass
+        def v2print(print_data):
             pass
     player = Player(player_type)
-    vprint(f"VERBOSE: {repr(player)}")
-    print(f"Host: Welcome {player.name}, to Keep OR Switch!")
+    v2print(f"DEBUG: {repr(player)}")
+    v1print(f"Host: Welcome {player.name}, to Keep OR Switch!")
 
 # Generate 3 boxes.
     boxes = [Box(_) for _ in range(1, 4)]
     winning_box = random.SystemRandom().choice(boxes)
     winning_box.has_car = True
-    vprint(f"VERBOSE: {boxes}")
+    v2print(f"DEBUG: {boxes}")
 
 # Player picks a box.
-    print(f"Host: I have placed a car in one of these three boxes, the other two contain a goat. {player.name}, please choose a box.")
+    v1print(f"Host: I have placed a car in one of these three boxes, the other two contain a goat. {player.name}, please choose a box.")
     chosen_box = player.choose_box(boxes)
     chosen_box.is_chosen = True
-    vprint(f"VERBOSE: Chosen box = {repr(chosen_box)}")
-    print(f"{player.name}: I would like box number {chosen_box.number} please.")
+    v2print(f"DEBUG: Chosen box = {repr(chosen_box)}")
+    v1print(f"{player.name}: I would like box number {chosen_box.number} please.")
 
 # Host Eliminates a losing box.
     for box in boxes:
         if box.is_chosen == False and box.has_car == False:
             box.is_opened = True
-            print(f"Host: I can reveal that box number {box.number} had a {'car' if box.has_car else 'goat'} inside it, and I will now remove it from the game.")
-            vprint(f"VERBOSE: Host removed {repr(box)}")
+            v1print(f"Host: I can reveal that box number {box.number} had a {'car' if box.has_car else 'goat'} inside it, and I will now remove it from the game.")
+            v2print(f"DEBUG: Host removed {repr(box)}")
             boxes.remove(box)
-            continue
+            break
 
 # Player chooses to keep or switch.
-    print(f"Host: Now, {player.name}, you have the option to keep your original box or switch to the remaining box.")
+    v1print(f"Host: Now, {player.name}, you have the option to keep your original box or switch to the remaining box.")
+    v2print(f"DEBUG: {boxes}")
     final_choice = player.keep_or_switch(boxes)
 
 # Result of the round
     round_result = final_choice.has_car
-    vprint(f"VERBOSE: Final choice is {repr(final_choice)}")
+    v2print(f"DEBUG: Final choice is {repr(final_choice)}")
     if final_choice == player.first_choice:
-        print(f"{player.name}: I have decided to keep my box.") 
+        v1print(f"{player.name}: I have decided to keep my box.") 
         if final_choice.has_car:
-            print(f"Host: Congratulations {player.name}, you have won a car!")
+            v1print(f"Host: Congratulations {player.name}, you have won a car!")
         else:
-            print(f"Host: Sorry {player.name}, you have won the goat.")
+            v1print(f"Host: Sorry {player.name}, you have won the goat.")
         return round_result
     else:
-        print(f"{player.name}: I have decided to switch my box!")
+        v1print(f"{player.name}: I have decided to switch my box!")
         if final_choice.has_car:
-            print(f"Host: Congratulations {player.name}, you have won a car!")
+            v1print(f"Host: Congratulations {player.name}, you have won a car!")
         else:
-            print(f"Host: Sorry {player.name}, you have won the goat.")
+            v1print(f"Host: Sorry {player.name}, you have won the goat.")
     return round_result
